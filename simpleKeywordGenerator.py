45bd824a4ec789a997a1f1a4282a6c2365d0e90e
Philip Garus <philipgarus97@gmail.com>
2021-11-25 17:25:03 +0100

    added glom test

--- simpleKeywordGenerator.py
+++ simpleKeywordGenerator.py
@@ -1,6 +1,9 @@
 import sys
 
 from keybert import KeyBERT
+import pandas as pd
+from pandas import json_normalize
+from glom import glom
 import json
 
 
@@ -17,8 +20,10 @@ def count_keys(selected_key, obj):
 
             if key == selected_key:
                 channeljoinmsg = "> has joined the channel"
+                emote = "emoji"
 
-               if channeljoinmsg not in str(obj[key]):
+               #if channeljoinmsg not in str(obj[key]):
+                if emote in str(obj[key]):
                     count += 1
 
             count += count_keys(selected_key, obj[key])
@@ -46,6 +51,30 @@ def get_all_attributes(selected_key, obj):
 
     return all_messages
 
+def pandatest(count,attribute, path):
+    #sets pandas output
+    #pd.set_option('display.max_rows', None)
+    #pd.set_option('display.max_columns', None)
+    #pd.set_option('display.width', None)
+    #pd.set_option('display.max_colwidth', -1)
+
+    with open(path,
+              "r") as read_file:
+        jsondata= json.loads(read_file.read())
+        #gets datagram
+        #pandanorm = json_normalize(jsondata['messages'])
+        #print(pandanorm)
+
+        #gets all messages
+        glomdata = glom(jsondata, ('messages',['text']))
+        #goes over datalist and removes elements with given string
+        glomdata[:] = [x for x in glomdata if "> has joined the channel" not in x]
+        #clean data from uninteded characters etc.
+        for i,x in enumerate(glomdata):
+            glomdata[i] = x.replace("\n"," ").replace("\\xa0", " ")
+
+
+        print(glomdata)
 
 # main function
 def main(count, attribute, path):
@@ -64,4 +93,4 @@ def main(count, attribute, path):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    pandatest(sys.argv[1], sys.argv[2], sys.argv[3])
